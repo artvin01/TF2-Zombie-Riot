d14433586f4675e7d6b300e28c087a946344c400
sq]s <100999666+squarebracket-s@users.noreply.github.com>
2026-04-03 19:35:52 +0200

    [npcs.py] hide Type_Hidden

--- modules/npcs.py
+++ modules/npcs.py
@@ -36,17 +36,18 @@ def parse():
     npc_list_html = ""
     for category, npc_list in NPCS_BY_CATEGORY.items():
         category_name = category.replace("Type_","").replace("-1","Unknown").title()
-        npc_list_html += util.fill_template(util.read("templates/npc/category_block_start.html"),{"key":category_name})
-        for npc in npc_list:
-            context = {
-                "npc_name": f"{modules.shared.get_npc_icon(npc["icon"])} {npc["name"]}",
-                "plugin_name": npc["plugin"],
-                "flags": map_flags(npc["flags"]),
-                "health": map_health(npc["health"]),
-                "desc": npc["description"],
-            }
-            npc_list_html += util.fill_template(util.read("templates/npc/npc_preview.html"),context)
-        npc_list_html += "</details>\n"
+        if category != "Type_Hidden":
+            npc_list_html += util.fill_template(util.read("templates/npc/category_block_start.html"),{"key":category_name})
+            for npc in npc_list:
+                context = {
+                    "npc_name": f"{modules.shared.get_npc_icon(npc["icon"])} {npc["name"]}",
+                    "plugin_name": npc["plugin"],
+                    "flags": map_flags(npc["flags"]),
+                    "health": map_health(npc["health"]),
+                    "desc": npc["description"],
+                }
+                npc_list_html += util.fill_template(util.read("templates/npc/npc_preview.html"),context)
+            npc_list_html += "</details>\n"
 
     context = {
         "npcdata": npc_list_html
