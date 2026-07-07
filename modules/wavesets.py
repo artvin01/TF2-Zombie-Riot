a596e9455e46f92afd274eecdf61a11b55eefcb2
sq]s <100999666+squarebracket-s@users.noreply.github.com>
2026-07-07 17:10:39 +0200

    remove some hardcoded stuff

--- modules/wavesets.py
+++ modules/wavesets.py
@@ -154,9 +154,6 @@ def parse_wave(wave_idx: int, wave_data: dict[str, Any], auto_wave_cash: bool):
                 })
             continue
 
-        if wave_entry_data["plugin"] == "npc_victorian_assault_vehicle":
-            wave_entry_data["plugin"] = "npc_vestan_assault_vehicle"
-
         """
         ? - use unknown
         // comment from TF2-Zombie-Riot code
@@ -550,8 +547,6 @@ def parse_common(cfg: dict[str, Any], filename: str, html_mapsets: str):
             wave_cfg = util.read(f"./TF2-Zombie-Riot/addons/sourcemod/configs/zombie_riot/{waveset_file}.cfg")
 
             # Waveset-specific typo fixes (or just removing lines that break the parser)
-            if waveset_file == "classic_alim&expi":
-                wave_cfg=wave_cfg.replace('			"plugin"	"120000000"',"") # overrides actual plugin name before it, which is why it has to be removed
             wave_cfg = unique_enemy_delays(wave_cfg)
 
             WAVESET_DATA: dict[str,Any] = vdf.loads(wave_cfg)["Waves"] # type: ignore[w]
